c663ae74b5d170b2f9372e4fad691a836fe765ec
Nikunj295 <nikunj.viradiya.160410116144@gmail.com>
2020-07-01 17:21:59 +0530

    Migrated the Datafetch and its other process to Option Component

--- src/component/Classification/Classification.js
+++ src/component/Classification/Classification.js
@@ -1,57 +1,12 @@
 import React, { Component } from 'react'
-import CustomPaginationActionsTable from "../Tables/Table";
-import ZoomPan from "../Charts/ZoomPan/ZoomPan";
 import ControlledOpenSelect from "../Select/ControlledOpenSelect"
 
 class Classification extends Component {
-    state = {
-        options:[],
-        values:[]
-    }
-    
-    componentDidMount(){
-        fetch("http://127.0.0.1:5000/classification")
-        .then(response=>response.json())
-        .then(
-            data => {
-                var myData = Object.keys(data).map(key => {
-                    return data[key];
-                })
-                this.setState({values:myData})
-                const newArray = [];
-                this.state.values.map(i=>{
-                    newArray.push({y: i['0']});
-                });	
-                const options = {
-                    theme: "light2", // "light1", "dark1", "dark2"
-                    animationEnabled: true,
-                    zoomEnabled: true,
-                    title: {
-                        text: "Try Zooming and Panning"
-                    },
-                    axisY: {
-                        includeZero: false
-                    },
-                    data: [{
-                        type: "area",
-                        dataPoints: newArray
-                    }]
-                }
-                this.setState({options})
-            }
-        )
-    }
-
     render() {
-        const {values,options} = this.state
         return (
             <div>
-                <div>
-                    <h1>Classification</h1>
-                    <ZoomPan options={options}/>
-                    <ControlledOpenSelect />
-                    <CustomPaginationActionsTable values={values}/>
-                </div>
+                <h1>Classification</h1>
+                <ControlledOpenSelect />
             </div>
         )
     }
--- src/component/Select/ControlledOpenSelect.js
+++ src/component/Select/ControlledOpenSelect.js
@@ -1,70 +1,77 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React ,{ Component }from 'react';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import Button from '@material-ui/core/Button';
 import TextField from "@material-ui/core/TextField"
+import CustomPaginationActionsTable from "../Tables/Table";
+import { withStyles } from "@material-ui/core/styles"
 
+const useStyles = theme => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 160,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+  button: {
+    margin: theme.spacing(3),
+  },
+})
 
-const useStyles = makeStyles((theme) => ({
-    formControl: {
-      margin: theme.spacing(1),
-      minWidth: 160,
-    },
-    selectEmpty: {
-      marginTop: theme.spacing(2),
-    },
-}))
-
-export default function ControlledOpenSelect() {
-    const classes = useStyles();
-    const [algorithm, setAlgorithm] = React.useState('');
-    const [rows, setRows] = React.useState(10);
-    const [cols, setCols] = React.useState(2);
-    const [cluster, setCluster] = React.useState(2);
-    const [kn, setKn] = React.useState(2);
-    const [maxTree, setMaxTree] = React.useState(5);
-    const [ntree, setNtree] = React.useState(30);
-    const [kernel, setKernel] = React.useState('');
+class ControlledOpenSelect extends Component {
+  state={
+    algorithm:'',
+    rows:10,
+    cols:2,
+    cluster:2,
+    kn:2,
+    maxTree:null,
+    ntree:20,
+    kernel:'',
+    values:[],
+    option:[]
+  }
   
-    const knear=()=>{
+  knear=()=>{
       return (
-        <TextField className={classes.formControl} 
+        <TextField className={this.props.classes.formControl} 
                  type="number" 
                  InputProps={{ inputProps: { min: 2 } }} 
                  id="standard-basic" 
                  label="Knear"
-                 value={kn}
+                 value={this.state.kn}
                  helperText="Number of Neighbour"
-                 onChange={(e)=>setKn(e.target.value)}
+                 onChange={(e)=>this.setState({kn:e.target.value})}
         />  
       )
     }
-    
-    const dtree = () =>{
+  
+  dtree = () =>{
       return (
-        <TextField className={classes.formControl} 
+        <TextField className={this.props.classes.formControl} 
                  type="number" 
                  id="standard-basic" 
                  label="Max-depth"
-                 value={maxTree}
+                 value={this.state.maxTree}
                  helperText="Number of level of tree"
-                 onChange={(e)=>setMaxTree(e.target.value)}
+                 onChange={(e)=>this.setState({maxTree:e.target.value})}
         />  
       )
     }
-    const svm = () =>{
+
+  svm = () =>{
       return (
         <>
-        <FormControl className={classes.formControl}>
+        <FormControl className={this.props.classes.formControl}>
           <InputLabel id="kernel">Kernel</InputLabel>
           <Select
             labelId="kernel"
             id="kernel"
-            value={kernel}
-            onChange={(e)=>setKernel(e.target.value)}
+            value={this.state.kernel}
+            onChange={(e)=>this.setState({kernel:e.target.value})}
           >
             <MenuItem value="linear">Linear</MenuItem>
             <MenuItem value="poly">Polynomial</MenuItem>
@@ -76,80 +83,118 @@ export default function ControlledOpenSelect() {
       )
     }
 
-    const rtree=()=>{
+  rtree=()=>{
       return (
-        <TextField className={classes.formControl} 
+        <TextField className={this.props.classes.formControl} 
                  type="number" 
                  InputProps={{ inputProps: { min: 10, max: 100 } }} 
                  id="standard-basic" 
                  label="No. of trees"
-                 value={ntree}
+                 value={this.state.ntree}
                  helperText="Some important text"
-                 onChange={(e)=>setNtree(e.target.value)}
+                 onChange={(e)=>this.setState({ntree:e.target.value})}
         />  
       )
     }
-
-    const handleChange = (event) => {
-      setAlgorithm(event.target.value);
-    };
     
-  return (
-    <div>
-      <FormControl className={classes.formControl}>
-        <InputLabel id="algorithm">Algorithm</InputLabel>
-        <Select
-          labelId="algorithm"
-          id="algorithm"
-          value={algorithm}
-          onChange={handleChange}
-        >
-          <MenuItem value="logisticRegression">Logistic Classifier</MenuItem>
-          <MenuItem value="knear">K-near Classifier</MenuItem>
-          <MenuItem value="naive">Naive</MenuItem>
-          <MenuItem value="dtree">Decision Tree Classifier</MenuItem>
-          <MenuItem value="rtree">Random Forest Tree</MenuItem>
-          <MenuItem value="svm">Support Vector Machine</MenuItem>
-        </Select>
-      </FormControl>
-      <TextField className={classes.formControl} 
-                 type="number" 
-                 InputProps={{ inputProps: { min: 10, max: 100000 } }} 
-                 id="standard-basic" 
-                 label="Rows"
-                 value={rows}
-                 helperText="Some important text"
-                 onChange={(e)=>setRows(e.target.value)}
-      />
-      <TextField className={classes.formControl} 
-                 type="number" 
-                 InputProps={{ inputProps: { min: 2, max: 50 } }} 
-                 id="standard-basic" 
-                 label="Columns" 
-                 value={cols}
-                 helperText="Some important text"
-                 onChange={(e)=>setCols(e.target.value)}
-      />
-      
-      <TextField className={classes.formControl} 
-                 type="number" 
-                 InputProps={{ inputProps: { min: 2, max: 10 } }} 
-                 id="standard-basic" 
-                 label="No. of Classification"
-                 value={cluster}
-                 helperText="How many class you want to distinguish?"
-                 onChange={(e)=>setCluster(e.target.value)}
-      />  
-      
-      {
-        algorithm==="knear" ? knear():
-        algorithm==="svm" ? svm():
-        algorithm==="dtree" ? dtree():
-        algorithm==="rtree" ? rtree():
-        null
-      }
-      <ZoomPan options={options}/>
-      <CustomPaginationActionsTable values={values}/>
-      </div>
-  );
+    getData = ()=>{
+      fetch("http://127.0.0.1:5000/classification")
+        .then(response=>response.json())
+        .then(
+            data => {
+                var myData = Object.keys(data).map(key => {
+                    return data[key];
+                })
+                this.setState({values:myData})
+                const newArray = [];
+                this.state.values.map(i=>{
+                    newArray.push({y: i['0']});
+                });	
+                const options = {
+                    theme: "light2", // "light1", "dark1", "dark2"
+                    animationEnabled: true,
+                    zoomEnabled: true,
+                    title: {
+                        text: "Try Zooming and Panning"
+                    },
+                    axisY: {
+                        includeZero: false
+                    },
+                    data: [{
+                        type: "area",
+                        dataPoints: newArray
+                    }]
+                }
+                this.setState({options})
+          })
+    }
+    
+    render(){
+      const { classes } = this.props
+      return (
+        <div>
+          <FormControl className={classes.formControl}>
+            <InputLabel id="algorithm">Algorithm</InputLabel>
+            <Select
+              labelId="algorithm"
+              id="algorithm"
+              value={this.state.algorithm}
+              onChange={(e)=>this.setState({algorithm:e.target.value})}
+            >
+              <MenuItem value="logisticRegression">Logistic Classifier</MenuItem>
+              <MenuItem value="knear">K-near Classifier</MenuItem>
+              <MenuItem value="naive">Naive</MenuItem>
+              <MenuItem value="dtree">Decision Tree Classifier</MenuItem>
+              <MenuItem value="rtree">Random Forest Tree</MenuItem>
+              <MenuItem value="svm">Support Vector Machine</MenuItem>
+            </Select>
+          </FormControl>
+          <TextField className={classes.formControl} 
+                    type="number" 
+                    InputProps={{ inputProps: { min: 10, max: 100000 } }} 
+                    id="standard-basic" 
+                    label="Rows"
+                    value={this.state.rows}
+                    helperText="Some important text"
+                    onChange={(e)=>this.setState({rows:e.target.value})}
+          />
+          <TextField className={classes.formControl} 
+                    type="number" 
+                    InputProps={{ inputProps: { min: 2, max: 50 } }} 
+                    id="standard-basic" 
+                    label="Columns" 
+                    value={this.state.cols}
+                    helperText="Some important text"
+                    onChange={(e)=>this.setState({cols:e.target.value})}
+          />    
+          <TextField className={classes.formControl} 
+                    type="number" 
+                    InputProps={{ inputProps: { min: 2, max: 10 } }} 
+                    id="standard-basic" 
+                    label="No. of Classification"
+                    value={this.state.cluster}
+                    helperText="How many class you want to distinguish?"
+                    onChange={(e)=>this.setState({cluster:e.target.value})}
+          />       
+          {
+            this.state.algorithm==="knear" ? this.knear():
+            this.state.algorithm==="svm" ? this.svm():
+            this.state.algorithm==="dtree" ? this.dtree():
+            this.state.algorithm==="rtree" ? this.rtree():
+            null
+          }
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={this.getData}
+            className={classes.button}
+          >
+            Create
+          </Button>
+          <CustomPaginationActionsTable values={this.state.values}/>
+        </div>
+    )
+  }
 }
+
+export default withStyles(useStyles)(ControlledOpenSelect)
